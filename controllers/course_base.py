e52f138f6e48214d00e0c09a59dfa7259587747e
Murshid <muhsink00000@gmail.com>
2024-05-24 16:36:43 +0530

    added total

--- controllers/course_base.py
+++ controllers/course_base.py
@@ -60,66 +60,78 @@ class LeadExcelReportCourseController(http.Controller):
             'valign': 'vcenter',
         })
         # get data for the report.
+        headers = []
         report_lines = report_id.get_course_reports()
         print(report_lines, 'report_lines')
+
         # prepare excel sheet styles and formats
         sheet = workbook.add_worksheet("courses")
         sheet.write(1, 0, 'No.', header_format)
         sheet.write(1, 1, 'Courses', header_format)
+        i = 1
         for source in lead_source:
-            sheet.write(1, 1 + source.id, source.name, header_format)
-        print(len(lead_source), 'len(lead_source)')
-        # sheet.write(1, len(lead_source)+2, 'Admission', header_format)
-        # sheet.write(1, len(lead_source)+3, 'Total', header_format)
-        # sheet.write(1, len(lead_source)+4, 'Percentage %', header_format)
+            i += 1
+            print(i, source.name , 'opp')
+            sheet.write(1, i, source.name, header_format)
+        # print(len(lead_source), 'len(lead_source)')
+        sheet.write(1, len(lead_source)+2, 'Admission', header_format)
+        sheet.write(1, len(lead_source)+3, 'Total', header_format)
+        sheet.write(1, len(lead_source)+4, 'Percentage %', header_format)
 
         row = 2
         number = 1
-        # write the report lines to the excel document
+
         for line in report_lines:
             sheet.set_row(row, 20)
             sheet.write(row, 0, number, )
             sheet.write(row, 1, line['lead_course'], )
+            j = 1
             for source in lead_source:
-                sheet.write(1, 1 + source.id, source.name, header_format)
+                j += 1
+                # sheet.write(1, i + 1, source.name, header_format)
 
                 search_count = request.env['leads.logic'].sudo().search_count(
                     [('id', 'in', datas.ids), ('leads_source', '=', source.id),
                      ('base_course_id', '=', line['course_id'])])
                 perc_course = round((search_count / total_count) * 100)
-                sheet.write(row, 1 + source.id, search_count, )
-            # sheet.write(row, len(lead_source)+2, line['adm_count'], admission)
-            # sheet.write(row, len(lead_source)+3, line['count'], total_format)
-            # sheet.write(row, len(lead_source)+4, line['prc_count'], percentage)
-
+                sheet.write(row, j, search_count, )
+            sheet.write(row, len(lead_source)+2, line['adm_count'], admission)
+            sheet.write(row, len(lead_source)+3, line['count'], total_format)
+            sheet.write(row, len(lead_source)+4, line['prc_count'], percentage)
+        #
             row += 1
             number += 1
         sheet.set_row(row, 20)
         sheet.write(row, 1, 'Admission', header_format)
+        ad = 1
         for adm in lead_source:
+            ad += 1
             adm_count = request.env['leads.logic'].sudo().search_count(
                 [('id', 'in', datas.ids), ('leads_source', '=', adm.id),('admission_status', '=', True),
                  ])
-            sheet.write(row, 1 + adm.id, adm_count, admission)
+            sheet.write(row, ad, adm_count, admission)
         row += 1
         sheet.write(row, 1, 'Total', header_format)
+        vd = 1
         for v in lead_source:
+            vd += 1
             search_count = request.env['leads.logic'].sudo().search_count(
                 [('id', 'in', datas.ids), ('leads_source', '=', v.id),
                  ])
-            sheet.write(row, 1 + v.id, search_count, total_format)
+            sheet.write(row,vd, search_count, total_format)
             sheet.write(row, len(lead_source) + 3, total_count, total_leads_format)
         row += 1
         sheet.write(row, 1, 'Percentage %', header_format)
+        ek = 1
         for e in lead_source:
+            ek += 1
             search_prc = request.env['leads.logic'].sudo().search_count(
                 [('id', 'in', datas.ids), ('leads_source', '=', e.id),
                  ])
             perc_source = round((search_prc / total_count) * 100)
-            sheet.write(row, 1 + e.id, perc_source, percentage)
+            sheet.write(row, ek, perc_source, percentage)
 
-            # perc_source = round((search_count / total_count) * 100)
-            # sheet.write(row, len(lead_source)+4, perc_source, percentage)
+
         row += 1
 
         workbook.close()
